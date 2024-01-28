31e474caf7d57d5ab91131ed6a1f43818a4de963
Nikola D Trajic <ntrajic@users.noreply.github.com>
2024-01-28 05:25:05 +0000

    added anewproj

---
+++ anewproj/src/lib.rs
@@ -0,0 +1,4 @@
+// anewproj/src/lib.rs is a libraray rust file w/ new add funct
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b       // return a+b
+}
---
+++ anewproj/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("using a library function add(x,y) in anewprj/src/lib.rs...");
+    let result = anewproj::add(1, 2);
+    println!("Hello, world! 1 + 2 = {}", result);
+}
