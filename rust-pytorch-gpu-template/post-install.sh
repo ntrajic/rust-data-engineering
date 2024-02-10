0ab9d990d82164581e99cf6ac69689600b32f2c1
Nikola D Trajic <ntrajic@users.noreply.github.com>
2024-02-10 00:21:18 +0000

    PYTORCH lib path in env for post-install.sh and

--- rust-pytorch-gpu-template/post-install.sh
+++ rust-pytorch-gpu-template/post-install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 /home/codespace/.python/current/bin/python -m venv /home/codespace/venv/
 source /home/codespace/venv/bin/activate
+
+# LIBTORCH paths
+export LIBTORCH=/workspaces/rust-pytorch-gpu-template/libtorch
+export LD_LIBRARY_PATH=${LIBTORCH}/lib:${LD_LIBRARY_PATH}
